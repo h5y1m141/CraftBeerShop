a6ffc8e580c14a7b5ce45f898bbad23708367083
hiroshi oyamada <h5y1m141@gmail.com>
2013-06-08 09:05:44 +0900

    お店情報登録する際に電話番号を登録する処理が抜けていたのでその修正

--- js/postToACSWithNode.js
+++ js/postToACSWithNode.js
@@ -1,8 +1,8 @@
 var https = require('https'),fs = require('fs'),qs = require('qs'),
-postdata,options,body,request,loginData,loginOptions,loginRequest,file,apiKey,loginID,loginPasswd;
+postdata,options,body,request,loginData,loginOptions,loginRequest,file,apiKey,loginID,loginPasswd,jsondb;
 file = fs.readFileSync("js/config.json");
 var json = JSON.parse(file.toString());
-apiKey = json.apiKey;
+apiKey = json.apiKey.production;
 loginID = json.login;
 loginPasswd = json.password;
 
@@ -22,74 +22,93 @@ loginOptions = {
   }
 };
 
-postdata = qs.stringify({
-  name:"THE ARCHIGRAM British Pub & Cafe",
-  state:"yamagata",
-  address:"山形市十日町2-1-35",
-  latitude:"38.246297",
-  longitude:"140.336086"
-});
+jsondb = JSON.parse(fs.readFileSync("js/minohWithGeocode.json"));
 
 
+// Test用に１件だけ登録するための処理
+  // postdata = qs.stringify({
+  //   name      :jsondb[10].name,
+  //   state     :jsondb[10].state,
+  //   phone_number:jsondb[10].phone_number,
+  //   latitude  :jsondb[10].latitude,
+  //   longitude :jsondb[10].longitude,
+  //   address   :jsondb[10].address
+  // });
+  // postToACS(postdata);
 
-//Initialise the variable that will store the response
-body='';
 
-
-
-//Now we're going to set up the request and the callbacks to handle the data
-loginRequest = https.request(loginOptions, function(response) {
-  //When we receive data, we want to store it in a string
-  response.on('data', function (chunk) {
-    body += chunk;
+for(var i=0;i<jsondb.length;i++){
+  postdata = qs.stringify({
+    name      :jsondb[i].name,
+    state     :jsondb[i].state,
+    phone_number:jsondb[i].phone_number,
+    latitude  :jsondb[i].latitude,
+    longitude :jsondb[i].longitude,
+    address   :jsondb[i].address
   });
-  //On end of the request, run what we need to
-  var postbody,request1,headers,result,cookie;
-  // ログイン時のセッション情報を維持するためのクッキーを取得
-  headers = response.headers["set-cookie"].toString().split("=");
-  result = headers[1].split(";");
-  cookie = result[0];
+  postToACS(postdata);
+}
 
-  options = {
-    hostname: 'api.cloud.appcelerator.com',
-    port: 443,
-    path: '/v1/places/create.json?key=' + apiKey,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Cookie': "_session_id=" + cookie,
-      'Content-Length': postdata.length
-    }
-  };
+function postToACS(postdata){
+  //Initialise the variable that will store the response
+  body='';
 
-  response.on('end',function() {
-    request1 = https.request(options, function(res) {
-      res.on('data', function (chunk1) {
-        postbody += chunk1;
-      });
-      res.on('end',function() {
-        console.log(postbody);
-      });
+  //Now we're going to set up the request and the callbacks to handle the data
+  loginRequest = https.request(loginOptions, function(response) {
+    //When we receive data, we want to store it in a string
+    response.on('data', function (chunk) {
+      body += chunk;
     });
-    request1.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
+    //On end of the request, run what we need to
+    var postbody,request1,headers,result,cookie;
+    // ログイン時のセッション情報を維持するためのクッキーを取得
+    headers = response.headers["set-cookie"].toString().split("=");
+    result = headers[1].split(";");
+    cookie = result[0];
+
+    options = {
+      hostname: 'api.cloud.appcelerator.com',
+      port: 443,
+      path: '/v1/places/create.json?key=' + apiKey,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': "_session_id=" + cookie,
+        'Content-Length': postdata.length
+      }
+    };
+
+    response.on('end',function() {
+      request1 = https.request(options, function(res) {
+        res.on('data', function (chunk1) {
+          postbody += chunk1;
+        });
+        res.on('end',function() {
+          console.log(postbody);
+        });
+      });
+      request1.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
+      request1.write(postdata);
+      request1.end();
     });
-    request1.write(postdata);
-    request1.end();
   });
-});
 
-//Now we need to set up the request itself.
-//This is a simple sample error function
-loginRequest.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
+  //Now we need to set up the request itself.
+  //This is a simple sample error function
+  loginRequest.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
 
 
-//Write our post data to the request
-loginRequest.write(loginData);
-//End the request.
-loginRequest.end();
+  //Write our post data to the request
+  loginRequest.write(loginData);
+  //End the request.
+  loginRequest.end();
+
+}
+
 
 
 
