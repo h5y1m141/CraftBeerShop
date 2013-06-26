3f4539c1d192ed3aa9455538ea4d6eb86c16d61f
hiroshi oyamada <h5y1m141@gmail.com>
2013-06-27 07:00:52 +0900

    お店情報の電話番号をキーにしてお店情報をマージするスクリプト

---
+++ js/mergeJSONDB.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+_ = require("../lib/underscore-1.4.3.min"),
+coedoFile = fs.readFileSync("js/coedoWithGeocode.json"),
+minohFile = fs.readFileSync("js/minohWithGeocode.json"),
+coedoDB = JSON.parse(coedoFile),
+minohDB = JSON.parse(minohFile),
+mergeList = coedoDB.concat(minohDB),
+result,
+item
+;
+
+// console.log(coedoDB.length);
+// console.log(minohDB.length);
+console.log(mergeList.length);
+result = _.groupBy(mergeList,function(data){
+  return data.phone_number;
+});
+
+fs.writeFileSync("js/mergeDataWithGeocode.json", JSON.stringify(result));
+// お店の電話番号にてグルーピングすることで、重複したデータが算出できる
+
+for(var i in result){
+  var length = result[i].length;
+  if(length == 2){
+    console.log(result[i]);
+  }
+
+}
