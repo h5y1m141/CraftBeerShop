13764a4a02bc7747e591fcad98081b89d72be280
hiroshi oyamada <h5y1m141@gmail.com>
2013-06-27 07:01:37 +0900

    Clous Services上の登録データを一括削除するスクリプト

--- js/deleteMasterData.js
+++ js/deleteMasterData.js
@@ -1,18 +1,44 @@
 var https = require('https'),fs = require('fs'),qs = require('qs'),
-file,json,apiKey,body,path,targetURL,loginRequest,_result,places;
+file,json,apiKey,body,path,targetURL,request,_result,places,idList,
+options,loginData,loginOptions,loginRequest,loginID,loginPasswd
+;
 
 file = fs.readFileSync("js/config.json");
 json = JSON.parse(file.toString());
 apiKey = json.apiKey.development;
+idList = [];
 
-placeSearch();
+loginID = json.login;
+loginPasswd = json.password;
+
+loginData = qs.stringify({
+  login:loginID,
+  password:loginPasswd
+});
+
+loginOptions = {
+  hostname: 'api.cloud.appcelerator.com',
+  port: 443,
+  path: '/v1/users/login.json?key=' + apiKey,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': loginData.length
+  }
+};
 
 
-function placeSearch(){
+placeSearch(function(idList){
+  console.log(idList);
+  deleteACSData(idList[0]);
+});
+
+
+function placeSearch(callback){
   body='';
-  path = '/v1/places/search.json?per_page=3&key=' + apiKey,
+  path = '/v1/places/search.json?per_page=2&key=' + apiKey,
   targetURL = "https://api.cloud.appcelerator.com" + path;
-  loginRequest = https.request(targetURL, function(response) {
+  request = https.request(targetURL, function(response) {
     response.on('data', function (chunk) {
       body += chunk;
     });
@@ -20,18 +46,67 @@ function placeSearch(){
        _result = JSON.parse(body);
       places = _result.response.places;
       for(var i=0;i<places.length;i++){
-        console.log(places[i].id);
+        idList.push(places[i].id);
+        console.log("GET SHOP DATA. name is:" + places[i].name);
       }
-
+      callback(idList);
     });
 
   });
 
-  loginRequest.on('error', function(e) {
+  request.on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
 
 
+  request.end();
+
+}
+
+function deleteACSData(placeID){
+  loginRequest = https.request(loginOptions, function(response) {
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    var postbody,request1,headers,result,cookie;
+    // ログイン時のセッション情報を維持するためのクッキーを取得
+    headers = response.headers["set-cookie"].toString().split("=");
+    result = headers[1].split(";");
+    cookie = result[0];
+    var body1='',
+    deleteRequest;
+    var options = {
+      hostname: 'api.cloud.appcelerator.com',
+      port: 443,
+      path: '/v1/places/delete.json?place_id=' + placeID + '&key=' + apiKey,
+      method: 'DELETE',
+      headers: {
+        'Cookie': "_session_id=" + cookie,
+        'Content-Length': 0
+      }
+    };
+    response.on('end', function (chunk) {
+
+      deleteRequest = https.request(options, function(res) {
+        res.on('data', function (chunk) {
+          body1 += chunk;
+        });
+        res.on('end',function(chunk){
+          console.log(body1);
+          console.log('done');
+        });
+      });
+
+      deleteRequest.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
+      deleteRequest.end();
+    });
+  });
+  loginRequest.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+  loginRequest.write(loginData);
   loginRequest.end();
 
 }
